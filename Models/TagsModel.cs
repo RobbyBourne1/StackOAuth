e5e3a003f5b8aa5181bf875cf164a7a77049683b
Robby Bourne <robbybourne1@gmail.com>
2017-09-18 12:38:50 -0400

    Initial Commit

---
+++ Models/TagsModel.cs
@@ -0,0 +1,14 @@
+using System;
+namespace StackOAuth.Models
+{
+    public class TagsModel
+    {
+        public int Id { get; set; }
+        public string TagName { get; set; }
+
+        public TagsModel()
+        {
+            
+        }
+    }
+}
