ab35a7557df3e7838cd7c27a813f68f4e8b465d8
Robby Bourne <robbybourne1@gmail.com>
2017-09-19 10:24:27 -0400

    Authorize set up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,32 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StackOAuth.Data;
 using StackOAuth.Models;
 
 namespace StackOAuth.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> um)
+        {
+            _context = context;
+            _userManager = um;
+        }
+
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Questions.ToList());
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
